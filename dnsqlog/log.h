a2d5e0d76cc1a8d774631a69eb87458b781d9106
Ashur <mikulasek@live.com>
2019-09-04 00:46:52 +0200

    LMDB path

--- dnsqlog/log.h
+++ dnsqlog/log.h
@@ -14,8 +14,8 @@
 #define C_MOD_LOGFILE "/var/log/whalebone/dnsqlog.log\0"
 #define C_MOD_LOGDEBUG "/var/log/whalebone/dnsqlogdebug.log\0"
 #define C_MOD_LOGAUDIT "/var/log/whalebone/dnsqlogaudit.log\0"
-#define C_MOD_LMDB_PATH "/mnt/c/var/whalebone/dnsqlog\0"
-//#define C_MOD_LMDB_PATH "/var/whalebone/dnsqlog\0"
+//#define C_MOD_LMDB_PATH "/mnt/c/var/whalebone/dnsqlog\0"
+#define C_MOD_LMDB_PATH "/var/whalebone/dnsqlog\0"
 
 void debugLog(const char *format, ...);
 void fileLog(const char *format, ...);
